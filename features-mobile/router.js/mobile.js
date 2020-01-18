c7546804f989532bb483e0d44e7bf378f200acb5
Ryan Igo <rigo@cfevans.com>
2020-01-18 16:08:24 -0500

    enable server

--- features-mobile/router.js/mobile.js
+++ features-mobile/router.js/mobile.js
@@ -2,6 +2,7 @@ const path = require('path');
 const view = path.join(__dirname, '../views');
 const express = require('express');
 const router = express.Router({mergeParams: true});
+const delay = require('delay');
 module.exports = router;
 
 router.get('/', async function(req,res){
@@ -9,4 +10,10 @@ router.get('/', async function(req,res){
 })
 router.get('/scanner', async function(req,res){
   res.render(`${view}/scanner`);
+})
+
+router.post('/scanner', async function(req,res){
+  delay('30000')
+  console.log(req.body);
+  res.json({'success': true})
 })
